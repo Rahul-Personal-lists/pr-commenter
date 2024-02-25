e08dc588c690840cad96e8797901cf6ad4e8d8e9
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-02-25 18:54:42 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -64,7 +64,7 @@ deleteComment() {
  curl -L \
   -X DELETE \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$repos/pulls/comments/$comment_Id
   
