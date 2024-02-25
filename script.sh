29d54ee01be25b0187c50e57c5f8d7569083ea6b
rahul <rahulpatidar0191@gmail.com>
2024-02-25 07:06:54 -0500

    fix env

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ createComment() {
 
   # Create a comment
 
-  gh pr comment $issueNumber --body $body
+  gh pr comment 10 --body "test"
 
   # comment=$(curl -s -H "Authorization: token $token" \
   #   -X POST -d "{\"body\": \"$body\"}" \
