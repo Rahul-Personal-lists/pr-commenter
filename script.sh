f76616c228b4cbb2f3be85051f777896ab1fc2f2
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-02-26 10:35:21 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -62,6 +62,7 @@ deleteComment() {
  #--method DELETE \
   # Delete the comment
    response=$(gh api \
+   --method DELETE \ 
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /repos/$repo/issues/10/comments/1963068283)
