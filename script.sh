48830c76c1e71d0cebea716a5e69ef6714b14431
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2024-02-26 10:40:34 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -59,22 +59,21 @@ deleteComment() {
     return
   fi
 
- #--method DELETE \
   # Delete the comment
-   response=$(gh api \
-    --method DELETE \ 
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/$repo/issues/10/comments/1963068283)
+  #  response=$(gh api \
+  #   --method DELETE \ 
+  #   -H "Accept: application/vnd.github+json" \
+  #   -H "X-GitHub-Api-Version: 2022-11-28" \
+  # /repos/$repo/issues/10/comments/1963068283)
   
   #/repos/Rahul-Personal-lists/copy-giftree/pulls/10/comments/1963068283)
   
-  # response=$(curl -L \
-  # -X DELETE \
-  # -H "Accept: application/vnd.github+json" \
-  # -H "Authorization: Bearer $GH_TOKEN" \
-  # -H "X-GitHub-Api-Version: 2022-11-28" \
-  # https://api.github.com/repos/Rahul-Personal-lists/copy-giftree/pulls/comments) ; STATUS1=$?
+  response=$(curl -L \
+  -X DELETE \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$repo/issues/10/comments/1963068283)
 
   echo "respone=$response"
   
